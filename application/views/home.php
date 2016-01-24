3f528e32360374a705eb21b4b8b4f43cc3d7e17f
msumenge <110424@student.sae.edu.au>
2016-01-24 20:09:15 +1000

    Added CodeIgniter v3.0.4

---
+++ application/views/home.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="Description" content="Sigma Digital provide Design, Development and Marketing services.">
+    <meta name="keywords" content="Sigma Digital, Web Design, Graphic Design, Digital marketing, freelance"/>
+    <meta name ="robots" content ="index"/>
+    <title>Sigma Digital | Design, Development & Marketing</title>
+    <link rel="icon" href="<?php echo base_url('images/favicon.ico'); ?>" type="image/x-icon" /><!-- image/png -->
+    
+    <!-- MDL -->
+    <link rel="stylesheet" href="<?php echo base_url('css/material.min.css'); ?>">
+    <script src="<?php echo base_url('js/material.min.js'); ?>"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    
+    <!-- jQuery -->
+    <script src="<?php echo base_url('js/jquery-2.1.4.min.js'); ?>"></script>
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url('css/font-awesome.min.css'); ?>">
+    
+    <!-- Site -->
+    <link rel="stylesheet" href="<?php echo base_url('css/main.css'); ?>">
+    <script src="<?php echo base_url('js/main.js'); ?>"></script> 
+    
+    <!--Favicon-->
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('images/favicon.png'); ?>">
+</head>
+<body>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header class="mdl-layout__header">
+            <div class="mdl-layout__header-row">
+                <span class="mdl-layout-title">Sigma Digital</span>
+                <div class="mdl-layout-spacer"></div>
+                <nav class="mdl-navigation mdl-layout--large-screen-only">
+                    <a class="mdl-navigation__link" href="#ourServices">Our Services</a>
+                    <a class="mdl-navigation__link" href="#portfolio">Portfolio</a>
+                    <a class="mdl-navigation__link" href="#about">About</a>
+                    <a class="mdl-navigation__link" href="#contact">Contact</a>
+                </nav>
+            </div>
+        </header>
+        <div class="mdl-layout__drawer">
+            <span class="mdl-layout-title">Sigma Digital</span>
+            <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="#ourServices">Our Services</a>
+                <a class="mdl-navigation__link" href="#portfolio">Porfolio</a>
+                <a class="mdl-navigation__link" href="#about">About</a>
+                <a class="mdl-navigation__link" href="#contact">Contact</a>
+            </nav>
+        </div>
+        <main class="mdl-layout__content">
+            <div class="page-content">
+                <div class="banner bg-cover"></div>
+                <div class="mdl-grid box-shadow">
+                    <div class="mdl-cell mdl-cell--12-col">
+                        <h2 class="text-center marg-top-25" id="ourServices">Our Services</h2>
+                    </div>
+                    <div class="mdl-cell mdl-cell--4-col mdl-cell--hide-tablet mdl-cell--hide-phone services design">
+                        <div class="text-center">
+                            <i class="fa fa-pencil animate"></i>
+                            <ul class="animate">
+                                <li>Logo design</li>
+                                <li>Business cards</li>
+                                <li>Email footer</li>
+                                <li>Company style guide</li>
+                                <li>Custom stationery</li>
+                            </ul>
+                        </div>
+                        <h3 class="text-center">Design</h3>
+                    </div>
+                    <div class="mdl-cell mdl-cell--4-col mdl-cell--hide-tablet mdl-cell--hide-phone services development">
+                        <div class="text-center">
+                            <i class="fa fa-code animate"></i>
+                            <ul class="animate">
+                                <li>Single page website</li>
+                                <li>Wordpress site</li>
+                                <li>Online shop</li>
+                                <li>Personal portfolio</li>
+                                <li>Custom web application</li>
+                                <li>Responsive web design</li>
+                            </ul>
+                        </div>
+                        <h3 class="text-center">Development</h3>
+                    </div>
+                    <div class="mdl-cell mdl-cell--4-col mdl-cell--hide-tablet mdl-cell--hide-phone services marketing">
+                        <div class="text-center">
+                            <i class="fa fa-line-chart animate"></i>
+                            <ul class="animate">
+                                <li>SEO</li>
+                                <li>Google Ad Words</li>
+                                <li>Google Analytics</li>
+                                <li>Social media management</li>
+                            </ul>
+                        </div>
+                        <h3 class="text-center">Marketing</h3>
+                    </div>
+                    <div class="mdl-cell mdl-cell--3-col-tablet mdl-cell--hide-desktop services design text-center mdl-card mdl-shadow--2dp">
+                        <h4>Design <i class="fa fa-pencil"></i></h4>
+                        <ul>
+                            <li>Logo design</li>
+                            <li>Business cards</li>
+                            <li>Email footer</li>
+                            <li>Company style guide</li>
+                            <li>Custom stationery</li>
+                        </ul>
+                    </div>
+                    <div class="mdl-cell mdl-cell--3-col-tablet mdl-cell--hide-desktop services development text-center mdl-card mdl-shadow--2dp">
+                        <h4>Development <i class="fa fa-code"></i></h4>
+                        <ul>
+                            <li>Single page website</li>
+                            <li>Wordpress site</li>
+                            <li>Online shop</li>
+                            <li>Personal portfolio</li>
+                            <li>Custom web application</li>
+                            <li>Responsive web design</li>
+                        </ul>
+                    </div>
+                    <div class="mdl-cell mdl-cell--2-col-tablet mdl-cell--hide-desktop services marketing text-center mdl-card mdl-shadow--2dp">
+                        <h4>Marketing <i class="fa fa-line-chart"></i></h4>
+                        <ul>
+                            <li>SEO</li>
+                            <li>Google Ad Words</li>
+                            <li>Google Analytics</li>
+                            <li>Social media management</li>
+                        </ul>
+                    </div>
+                    <div class="mdl-cell mdl-cell--12-col padd-top-25"></div>
+                </div>
+                <div class="mdl-grid">
+                    <div class="mdl-cell mdl-cell--12-col">
+                        <h2 class="text-center marg-top-25" id="portfolio">Portfolio</h2>
+                    </div>
+                    <div class="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet tiles tile-1 zoom-on-hover animate">
+                    </div>
+                    <div class="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet tiles">
+                        <div class="mdl-grid mdl-grid--no-spacing">
+                            <div class="mdl-cell mdl-cell--12-col mdl-cell--4-col-tablet tile-2 zoom-on-hover animate">
+                                <a href="http://www.studiosapphire.co"></a>
+                            </div>
+                            <div class="mdl-cell mdl-cell--12-col mdl-cell--4-col-tablet tile-3 zoom-on-hover animate">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mdl-cell mdl-cell--12-col padd-top-25"></div>
+                </div>
+                <div class="mdl-grid box-shadow">
+                    <div class="mdl-cell mdl-cell--12-col">
+                        <h2 class="text-center marg-top-25" id="about">About</h2>
+                    </div>
+                    <div class="mdl-grid padd-bottom-25">
+                        <div class="mdl-cell mdl-cell--4-col-desktop mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-cell--middle person-photo">
+                            <div class="bg-cover ally"></div>
+                        </div>
+                        <div class="mdl-cell mdl-cell--7-col-desktop mdl-cell--4-col-tablet mdl-cell--4-col-phone person-desc">
+                            <h4 class="marg-top-0">Ally</h4>
+                            <p>Hey everyone! I have recently finished studying a Bachelor of Design (Majoring in Web) at SAE Institute, Brisbane. I specialise in Web Design, but also can assist you with Graphic Design and Digital Marketing. I have a bubbly personality and am easy to work with. To learn more about me, check out <a href="http://www.allyeastman.com.au" target="_blank">my website</a>, <a href="http://www.theloop.com.au/aeastman/portfolio" target="_blank">full portfolio</a> or <a href="http://www.allyeastman.tumblr.com" target="_blank">  blog.</a> </p>
+                        </div>
+                    </div>
+                    <div class="mdl-grid padd-top-25">
+                        <div class="mdl-cell mdl-cell--4-col-desktop mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-cell--middle person-photo">
+                            <div class="bg-cover maryo"></div>
+                        </div>
+                        <div class="mdl-cell mdl-cell--7-col-desktop mdl-cell--4-col-tablet mdl-cell--4-col-phone person-desc">
+                            <h4 class="marg-top-0">Maryo</h4>
+                            <p>
+                                Hello world... I'm a web development student at SAE Institute Brisbane, currently studying a Bachelor of Interactive Technologies. I have about one year experience in the front-end and back-end web developement, and I also have some basic knowledge in software programming. Check me out at <a href="http://www.maryo.info" target="_blank">www.maryo.info</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="mdl-cell mdl-cell--12-col padd-top-25"></div>
+                </div>
+                <div class="mdl-grid">
+                    <div class="mdl-cell mdl-cell--12-col">
+                        <h2 class="text-center marg-top-25" id="contact">Contact Us</h2>
+                    </div>
+                    <div class="mdl-cell mdl-cell--6-col-desktop  mdl-cell--4-col-tablet  mdl-cell--4-col-phone">
+                        <div class="mdl-grid">
+                            <div class="mdl-cell mdl-cell--12-col">
+                                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                    <input class="mdl-textfield__input" type="text" id="form-name" name="form-name" />
+                                    <label class="mdl-textfield__label" for="form-name">Name</label>
+                                </div>
+                            </div>
+                            <div class="mdl-cell mdl-cell--12-col">
+                                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                    <input class="mdl-textfield__input" type="text" id="form-phone" name="form-phone" pattern="-?[0-9]*(\.[0-9]+)?"/>
+                                    <label class="mdl-textfield__label" for="form-phone">Phone</label>
+                                    <span class="mdl-textfield__error">Input is not a number!</span>
+                                </div>
+                            </div>
+                            <div class="mdl-cell mdl-cell--12-col">
+                                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                    <input class="mdl-textfield__input" type="text" id="form-email" name="form-email" />
+                                    <label class="mdl-textfield__label" for="form-email">Email</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mdl-cell mdl-cell--6-col-desktop  mdl-cell--4-col-tablet  mdl-cell--4-col-phone">
+                        <div class="mdl-grid">
+                            <div class="mdl-cell mdl-cell--12-col">
+                                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                    <textarea class="mdl-textfield__input" type="text" rows="10" id="form-msg" name="form-msg"></textarea>
+                                    <label class="mdl-textfield__label" for="form-msg">Message or enquiry...</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mdl-cell mdl-cell--12-col text-center">
+                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                        send
+                        </button>
+                    </div>
+                    <div class="mdl-cell mdl-cell--12-col padd-top-25"></div>
+                </div>
+                <footer class="mdl-mega-footer">
+                    <!--div class="mdl-mega-footer__middle-section">
+                        <div class="mdl-mega-footer__drop-down-section">
+                          <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+                          <h1 class="mdl-mega-footer__heading">Features</h1>
+                          <ul class="mdl-mega-footer__link-list">
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Terms</a></li>
+                            <li><a href="#">Partners</a></li>
+                            <li><a href="#">Updates</a></li>
+                          </ul>
+                        </div>
+
+                        <div class="mdl-mega-footer__drop-down-section">
+                          <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+                          <h1 class="mdl-mega-footer__heading">FAQ</h1>
+                          <ul class="mdl-mega-footer__link-list">
+                            <li><a href="#">Questions</a></li>
+                            <li><a href="#">Answers</a></li>
+                            <li><a href="#">Contact us</a></li>
+                          </ul>
+                        </div>
+                    </div-->
+                    <div class="mdl-mega-footer__bottom-section">
+                        <div class="mdl-logo">Copyright &copy; 2015 Sigma Digital</div>
+                        <ul class="mdl-mega-footer__link-list">
+                            <li><a href="#ourServices">Our Services</a></li>
+                            <li><a href="#portfolio">Portfolio</a></li>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </div>
+                </footer>
+            </div>
+        </main>
+    </div>
+    <script>
+        $(document).ready(function() {
+
+        });
+    </script>
+</body>
+</html>
